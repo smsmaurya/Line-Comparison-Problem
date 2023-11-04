979ba757a0ebc913a84592447e5c37fc68a551dd
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-04 19:31:32 +0530

    Welcome message

---
+++ src/com/bridgeLabz/LineComparisionProgram.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class LineComparisionProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    }
+}
